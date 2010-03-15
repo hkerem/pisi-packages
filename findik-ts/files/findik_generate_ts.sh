ddce564d53848ebcb1b629337f9a7fd07e3388fe
root <root@kerem-pardus.(none)>
2010-03-15 19:13:40 +0200

    initial commit for package findik timestamping utility

---
+++ findik-ts/files/findik_generate_ts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DB_REPLY=$(mysql --user=root --password= findik -e "select * from btk_user_info;" --batch | tail -1)
+TS_USERNAME=$(echo $DB_REPLY| awk '{ print $1 }' )
+TS_PASSWORD=$(echo $DB_REPLY| awk '{ print $2 }' )
+
+java -jar /var/lib/findik/ts/ZamaneConsole-1.1.9.jar -z $1 http://tzd.kamusm.gov.tr 80 $TS_USERNAME $TS_PASSWORD
