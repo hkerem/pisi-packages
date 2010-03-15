d6c209ece822ad81cf74944ffe655677062a7d38
Burak OGUZ <burak@medrateknoloji.com>
2010-03-15 10:59:13 +0200

    bug fix in pyinotify script

--- findik-ts/files/findik-pyinotify.py
+++ findik-ts/files/findik-pyinotify.py
@@ -27,7 +27,7 @@ mask = pyinotify.IN_CREATE  # watched events
 
 class HandleEvents(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
-        if event.pathname.find(".gz") != -1 and event.pathname.find(".zd") == -1:
+        if event.pathname.find(".gz") != -1 and event.pathname.find(".zd") == -1 and event.pathname.find("ZDERROR") == -1:
             os.system("/usr/bin/findik-generate-ts %s" % (event.pathname))
 
 p = HandleEvents()
