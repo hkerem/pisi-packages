5b3453966729e28712042654bf69a6931a649a9f
hkerem <kerem@medratech.com>
2010-03-28 20:42:22 +0300

    Version update

--- findik/actions.py
+++ findik/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="hkerem-findik-c70472c"
+WorkDir="hkerem-findik-9e53fd8"
 
 def setup():
     shelltools.system("./autogen.sh")
