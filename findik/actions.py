638bd5b310de31cc8ef0abe42814cd3cf43ef85e
hkerem <kerem@medratech.com>
2010-04-06 21:46:53 +0300

    Version bump

--- findik/actions.py
+++ findik/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="hkerem-findik-6611dfd"
+WorkDir="hkerem-findik-38cd662"
 
 def setup():
     shelltools.system("./autogen.sh")
